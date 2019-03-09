dd9bd44c2c8de40d48a86092b7a2bd2dbf299db6
adam.galloway <admgalloway@gmail.com>
2019-03-09 15:52:37 +0000

    adding enums for Error and Result values

---
+++ Src/AG.Common.ServiceResponsePattern/Enums/ServiceResponseErrorCode.cs
@@ -0,0 +1,14 @@
+﻿namespace AG.Common.ServiceResponsePattern.Enums
+{
+    /// <summary>Defines a list of error codes to communicate common errors found in services, and communicated back to callers.</summary>
+    /// <remarks>These are mapped to HTTP status codes for convience only, allowing for simpler wrapping at the API level.</remarks>
+    public enum ServiceResponseErrorCode
+    {
+        NoError                 = 0,
+        ValidationError         = 400,
+        AuthorizationError      = 401,
+        AuthenticationError     = 403,
+        NotFound                = 404,
+        SystemError             = 500
+    }
+}
---
+++ Src/AG.Common.ServiceResponsePattern/Enums/ServiceResponseResult.cs
@@ -0,0 +1,10 @@
+﻿namespace AG.Common.ServiceResponsePattern.Enums
+{
+    /// <summary>Defines a list of possible results from a service response.</summary>
+    /// <remarks>Details of failures are communicated via the ServiceResponseErrorCode enum.</remarks>
+    public enum ServiceResponseResult
+    {
+        Successful              = 1,
+        Failed                  = 2
+    }
+}
