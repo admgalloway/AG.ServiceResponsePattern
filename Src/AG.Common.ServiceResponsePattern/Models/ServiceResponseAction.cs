1a76bd27b9d2c3cb6740b41f14a34af6e2ed3cd0
adam.galloway <admgalloway@gmail.com>
2019-03-09 15:53:35 +0000

    adding models for Actions and Errors

---
+++ Src/AG.Common.ServiceResponsePattern/Models/ServiceResponseAction.cs
@@ -0,0 +1,30 @@
+﻿namespace AG.Common.ServiceResponsePattern.Models
+{
+    /// <summary>Defines an an action which a caller can carry out following the request they have just made. For example, this may
+    /// a retry action for a failed request, or a follow-up action for new resources that has just been created, etc.
+    /// </summary>
+    public class ServiceResponseAction
+    {
+        public ServiceResponseAction(string id, string title, string href, string method)
+        {
+            Id = id;
+            Title = title;
+            Href = href;
+            Method = method;
+        }
+
+        // this needs some thought. These are very specifically api-driven properties, which a service might not know.
+
+        /// <summary>A unique identity for the action.</summary>
+        public string Id { get; set; }
+
+        /// <summary>A user friendly label for the action.</summary>
+        public string Title { get; set; }
+
+        /// <summary>An absoulte url identifiy the address of the resource.</summary>
+        public string Href { get; set; }
+
+        /// <summary>The method type supported by the resource.</summary>
+        public string Method { get; set; }
+    }
+}
---
+++ Src/AG.Common.ServiceResponsePattern/Models/ServiceResponseError.cs
@@ -0,0 +1,25 @@
+﻿namespace AG.Common.ServiceResponsePattern.Models
+{
+    /// <summary>Represents an error related to input from a caller (typically provided by a user).</summary>
+    public class ServiceResponseError
+    {
+        public ServiceResponseError(string fieldName, string errorCode)
+        {
+            FieldName = fieldName;
+            ErrorCode = errorCode;
+        }
+
+        /// <summary>Indicates which input field this error relates to.</summary>
+        public string FieldName { get; set; }
+
+        /// <summary>Indicates why the request failed. Callers that present details to users should translate these code into user-friendly messages.</summary>
+        /// <remarks>This implentation now uses a string value. Enums were good for a few generic error codes, but they were very restrictive, required a lot of
+        /// effort to add new types, and did not handle project-specific error codes well. Project-specific may (and should) be used to enforce , then cast to 
+        /// strings when supplied to this class.</remarks>
+        public string ErrorCode { get; set; }
+
+        /// ErrorMessage has been removed from this class. Specifying messages for users at this 
+        /// level is too rigid. The preferred approach is to keep a dictionary of error codes used
+        /// within a project so that accompanying error messages can be managed independently.
+    }
+}
