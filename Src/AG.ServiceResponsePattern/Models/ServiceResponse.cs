7af4df7fdcbb67fc1a307dd0cf6d7b0edd97c643
adam.galloway <admgalloway@gmail.com>
2019-03-10 01:47:12 +0000

    Made constructor internal to force object creation through Factory

--- Src/AG.ServiceResponsePattern/Models/ServiceResponse.cs
+++ Src/AG.ServiceResponsePattern/Models/ServiceResponse.cs
@@ -7,8 +7,11 @@ namespace AG.ServiceResponsePattern.Models
     /// All exceptions within the service call should be logged, and the overall outcome of the method reported back to callers via the Result property.
     /// This implimentation does not return content if the method was successful.
     /// </summary>
-    public class ServiceResponse
+    public partial class ServiceResponse
     {
+        /// <summary>Restrict creation of objects to internal usage - force consumers to use Factory to reliably populate object</summary>
+        internal ServiceResponse() { }
+
         /// <summary>Indicates if the request was successful or not.</summary>
         public ServiceResponseResult Result { get; set; }
 
@@ -33,9 +36,12 @@ namespace AG.ServiceResponsePattern.Models
     /// All exceptions within the service call should be logged, and the overall outcome of the method reported back to callers via the Result property.
     /// This implimentation accepts a type param and, if successful, returns a value of that type via the Content property.
     /// </summary>
-    public class ServiceResponse<T> : ServiceResponse
+    public partial class ServiceResponse<ResponseType> : ServiceResponse
     {
+        /// <summary>Restrict creation of objects to internal usage - force consumers to use Factory to reliably populate object</summary>
+        internal ServiceResponse() { }
+
         /// <summary>For successfull requests, this contains the content returned by the request.</summary>
-        public T Content { get; set; }
+        public ResponseType Content { get; set; }
     }
 }
